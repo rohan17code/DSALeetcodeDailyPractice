d76fb62edee6ebeed595c9d738104ae10ac8da63
Rohan Tyagi <rohantyagi539@gmail.com>
2026-07-10 04:41:32 +0530

    Time: 2 ms (99.95%), Space: 52.7 MB (40.23%) - LeetHub

--- LeetCode/Medium/0229-majority-element-ii/0229-majority-element-ii.java
+++ LeetCode/Medium/0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,15 +1,33 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> result = new ArrayList<>();
-        for(int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        for(Integer x : map.keySet()) {
-            if(map.get(x) > nums.length / 3) {
-                result.add(x);
+        int candidate1 = 0, candidate2 = 0;
+        int cnt1 = 0, cnt2 = 0;
+        for(int num : nums) {   // First Pass: Find the two potential candidates
+            if(num == candidate1) {
+                cnt1++;
+            } else if(num == candidate2) {
+                cnt2++;
+            } else if(cnt1 == 0) {
+                candidate1 = num;
+                cnt1 = 1;
+            } else if(cnt2 == 0) {
+                candidate2 = num;
+                cnt2 = 1;
+            } else {
+                cnt1--;
+                cnt2--;
             }
         }
-        return result;
+        // finding real freq of the candidates
+        cnt1 = 0;
+        cnt2 = 0;
+        for(int num : nums) {
+            if(num == candidate1) cnt1++;
+            else if(num == candidate2) cnt2++;
+        }
+        List<Integer> ans = new ArrayList<>();
+        if(cnt1 > nums.length / 3) ans.add(candidate1);
+        if(cnt2 > nums.length / 3) ans.add(candidate2);
+        return ans;
     }
 }
