356f399b14659d9304df8dd30eb170247b8cc5ce
Rohan Tyagi <rohantyagi539@gmail.com>
2026-07-11 01:08:15 +0530

    Time: 0 ms (100%), Space: 43.7 MB (24.78%) - LeetHub

---
+++ LeetCode/Medium/0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int mid = 0, low = 0, high = nums.length - 1;
+        while(mid <= high) {
+            if(nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if(nums[mid] == 1) mid++;
+            else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+
+            }
+        }
+    }
+}
