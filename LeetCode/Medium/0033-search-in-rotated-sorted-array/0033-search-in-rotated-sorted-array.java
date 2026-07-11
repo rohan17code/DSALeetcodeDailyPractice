6cd08c26c1392f01553a1842ccc8f19107359102
Rohan Tyagi <rohantyagi539@gmail.com>
2026-07-12 03:02:55 +0530

    Time: 0 ms (100%), Space: 44 MB (11.24%) - LeetHub

--- LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,9 +1,25 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int ans = -1;
-        for(int i = 0; i<nums.length; i++) {
-            if(nums[i] == target) ans = i;
+        int st = 0, end = nums.length - 1;
+        while(st <= end) {
+            int mid = st + (end - st) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            if(nums[st] <= nums[mid]) {   //left sorted
+                if(nums[st] <= target && target <= nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    st = mid + 1;  // go right
+                }
+            } else {   //right sorted
+                if(nums[mid] <= target && target <= nums[end]) {
+                    st = mid + 1;
+                } else {
+                    end = mid - 1; // go left
+                }
+            }
         }
-        return ans;
+        return -1;
     }
 }
