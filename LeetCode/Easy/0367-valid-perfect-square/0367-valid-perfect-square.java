866631510391041f8fd0cc176569ea4a87ce4bf7
Rohan Tyagi <rohantyagi539@gmail.com>
2026-07-20 02:51:35 +0530

    Time: 0 ms (100%), Space: 42.2 MB (38.98%) - LeetHub

--- LeetCode/Easy/0367-valid-perfect-square/0367-valid-perfect-square.java
+++ LeetCode/Easy/0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,7 +1,15 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        int n = (int) Math.sqrt(num);
-        if(n * n == num) return true;
-        else return false;
+        if(num == 1) return true;
+        long left = 1;
+        long right = num;
+        while(left <= right) {
+            long mid = left + (right - left) / 2;
+            long square = mid * mid;
+            if(square == num) return true;
+            else if(square < num) left = mid + 1;
+            else right = mid - 1;
+        }
+        return false;
     }
 }
