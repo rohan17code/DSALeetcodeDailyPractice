4de613eac5865cf93b938132fa376f662403b31a
Rohan Tyagi <rohantyagi539@gmail.com>
2026-07-29 17:15:59 +0530

    Time: 655 ms (14.61%), Space: 0B (100%) - LeetHub

---
+++ LeetCode/Medium/0178-rank-scores/0178-rank-scores.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT s1.score, COUNT(DISTINCT s2.score) AS 'rank'
+FROM Scores s1
+INNER JOIN Scores s2
+ON s1.score <= s2.score
+GROUP BY s1.id, s1.score
+ORDER BY s1.score DESC
