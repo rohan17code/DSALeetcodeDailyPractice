ba1d8c6b18cda00c8990596ad470d97bc475904f
Rohan Tyagi <rohantyagi539@gmail.com>
2026-08-01 04:55:13 +0530

    Time: 586 ms (13.93%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ LeetCode/Medium/0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -3,12 +3,10 @@ BEGIN
 SET N = N - 1;
   RETURN (
       # Write your MySQL query statement below.
-    SELECT (
-        SELECT DISTINCT salary
-        FROM Employee
-        ORDER BY salary DESC
-        LIMIT 1 OFFSET N
-    )
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET N
   );
 END
 
