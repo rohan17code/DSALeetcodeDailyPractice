496b8ceed7a1f601893b5cd281cb3bcad32c15ca
Rohan Tyagi <rohantyagi539@gmail.com>
2026-08-10 02:07:39 +0530

    Time: 0 ms (100%), Space: 49.7 MB (13.16%) - LeetHub

--- LeetCode/Medium/0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ LeetCode/Medium/0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -1,20 +1,5 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    private int leftHeight(TreeNode root) {
+    private int leftH(TreeNode root) {
         int h = 0;
         while(root != null) {
             root = root.left;
@@ -22,7 +7,7 @@ class Solution {
         }
         return h;
     }
-    private int rightHeight(TreeNode root) {
+    private int rightH(TreeNode root) {
         int h = 0;
         while(root != null) {
             root = root.right;
@@ -31,10 +16,10 @@ class Solution {
         return h;
     }
     public int countNodes(TreeNode root) {
-        int leftHeight = leftHeight(root);
-        int rightHeight = rightHeight(root);
-        if(leftHeight == rightHeight) {
-            return (int) Math.pow(2, leftHeight) - 1;
+        int leftH = leftH(root);
+        int rightH = rightH(root);
+        if(leftH == rightH) {
+            return (int) Math.pow(2, leftH) - 1;
         } else {
             return countNodes(root.left) + countNodes(root.right) + 1;
         }
