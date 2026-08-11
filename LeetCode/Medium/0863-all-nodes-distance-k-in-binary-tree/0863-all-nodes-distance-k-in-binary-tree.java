c6ca1954892aa406de4b4b045cdb857758236c8f
Rohan Tyagi <rohantyagi539@gmail.com>
2026-08-12 01:51:22 +0530

    Time: 13 ms (49.37%), Space: 43.9 MB (86.35%) - LeetHub

---
+++ LeetCode/Medium/0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,53 @@
+
+class Solution {
+    private void makeParentPointer(TreeNode root, Map<TreeNode, TreeNode> parent) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()) {
+            TreeNode curr = q.poll();
+            if(curr.left != null) {
+                parent.put(curr.left, curr);
+                q.offer(curr.left);
+            }
+            if(curr.right != null) {
+                parent.put(curr.right, curr);
+                q.offer(curr.right);
+            }
+        }
+    }
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        Map<TreeNode, TreeNode> parent = new HashMap<>();
+        makeParentPointer(root, parent);
+        Queue<TreeNode> q = new LinkedList<>();
+        Set<TreeNode> visited = new HashSet<>();
+        q.offer(target);
+        visited.add(target);
+        int dist = 0;
+        while(!q.isEmpty()) {
+            if(dist == k) break;
+            int size = q.size();
+            for(int i = 0; i<size; i++) {
+                TreeNode curr = q.poll();
+                if(curr.left != null && !visited.contains(curr.left)) {
+                    q.offer(curr.left);
+                    visited.add(curr.left);
+                }
+                if(curr.right != null && !visited.contains(curr.right)) {
+                    q.offer(curr.right);
+                    visited.add(curr.right);
+                }
+                TreeNode p = parent.get(curr); // curr ke parent ko nikalna
+                if(p != null && !visited.contains(p)) {
+                    q.offer(p);
+                    visited.add(p);
+                }
+            } 
+            dist++;
+        }
+        List<Integer> ans = new ArrayList<>();
+        while(!q.isEmpty()) {
+            ans.add(q.poll().val);
+        }
+        return ans;
+    }
+}
