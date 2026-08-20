95deaad6054c442e23ab79b4fde9a6ea2036e107
Rohan Tyagi <rohantyagi539@gmail.com>
2026-08-21 01:59:33 +0530

    Time: 0 ms (100%), Space: 42 MB (73.14%) - LeetHub

--- LeetCode/Easy/0292-nim-game/0292-nim-game.java
+++ LeetCode/Easy/0292-nim-game/0292-nim-game.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean canWinNim(int n) {
-        if(n % 4 == 0) return true;
-        else return false;
+        if(n % 4 == 0) return false;
+        else return true;
     }
 }
