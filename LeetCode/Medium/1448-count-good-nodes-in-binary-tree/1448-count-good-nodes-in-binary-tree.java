876103d8d3ec215dc87c0323ed495e6bcf96f9b9
Rohan Tyagi <rohantyagi539@gmail.com>
2026-09-11 02:53:00 +0530

    Time: 2 ms (92.61%), Space: 57.2 MB (29.93%) - LeetHub

--- LeetCode/Medium/1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ LeetCode/Medium/1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -17,12 +17,10 @@ class Solution {
     private int cnt = 0;
     private void dfs(TreeNode curr, int max) {
         if(curr == null) return;
-        if(curr.val >= max) {
-            cnt++;
-        }
+        if(curr.val >= max) cnt++;
         max = Math.max(max, curr.val);
         dfs(curr.left, max);
-        dfs(curr.right, max); 
+        dfs(curr.right, max);
     }
     public int goodNodes(TreeNode root) {
         dfs(root, root.val);
