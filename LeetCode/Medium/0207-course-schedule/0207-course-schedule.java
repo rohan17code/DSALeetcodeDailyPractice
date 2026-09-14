58e331de6dda63797cda8a1463498c6b1d17368e
Rohan Tyagi <rohantyagi539@gmail.com>
2026-09-15 04:20:43 +0530

    Time: 29 ms (5.12%), Space: 46.6 MB (97.06%) - LeetHub

--- LeetCode/Medium/0207-course-schedule/0207-course-schedule.java
+++ LeetCode/Medium/0207-course-schedule/0207-course-schedule.java
@@ -1,16 +1,16 @@
 class Solution {
-    private static void CalInDeg(int [][] prerequisites, int[] inDegree) {
+    private static void CalInDeg(int[][] prerequisites, int[] inDeg) {
         for(int i = 0; i<prerequisites.length; i++) {
             int dest = prerequisites[i][0];
-            inDegree[dest]++;
-        }        
+            inDeg[dest]++;
+        }
     }
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] inDegree = new int[numCourses];
-        CalInDeg(prerequisites, inDegree);
+        int[] inDeg = new int[numCourses];
+        CalInDeg(prerequisites, inDeg);
         Queue<Integer> q = new LinkedList<>();
-        for(int i = 0; i<inDegree.length; i++) {
-            if(inDegree[i] == 0) {
+        for(int i = 0; i<inDeg.length; i++) {
+            if(inDeg[i] == 0) {
                 q.offer(i);
             }
         }
@@ -20,16 +20,14 @@ class Solution {
             cnt++;
             for(int i = 0; i<prerequisites.length; i++) {
                 if(prerequisites[i][1] == curr) {
-                int dest = prerequisites[i][0];
-                inDegree[dest]--;
-                if(inDegree[dest] == 0) {
-                    q.offer(dest);
-                }
-                
-            }
+                    int dest = prerequisites[i][0];
+                    inDeg[dest]--;
+                    if(inDeg[dest] == 0) {
+                        q.offer(dest);
+                    }
+                } 
             }
         }
-        if(cnt == numCourses) return true;
-        else return false;
+        return cnt == numCourses;
     }
 }
