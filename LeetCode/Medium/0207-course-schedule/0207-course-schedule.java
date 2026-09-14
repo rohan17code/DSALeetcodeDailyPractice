129dbf24fd3f881d4b627a579432e27a3e7b47ac
Rohan Tyagi <rohantyagi539@gmail.com>
2026-09-15 05:22:09 +0530

    Time: 3 ms (99.81%), Space: 47.4 MB (21.28%) - LeetHub

--- LeetCode/Medium/0207-course-schedule/0207-course-schedule.java
+++ LeetCode/Medium/0207-course-schedule/0207-course-schedule.java
@@ -1,33 +1,38 @@
 class Solution {
-    private static void CalInDeg(int[][] prerequisites, int[] inDeg) {
-        for(int i = 0; i<prerequisites.length; i++) {
-            int dest = prerequisites[i][0];
-            inDeg[dest]++;
+    private boolean isCycle(ArrayList<Integer>[] graph, int curr, boolean[] vis, boolean[] stack) {
+        vis[curr] = true;
+        stack[curr] = true;
+        for(int i = 0; i<graph[curr].size(); i++) {
+            int dest = graph[curr].get(i);
+            if(stack[dest]) {
+                return true;
+            }
+            if(!vis[dest] && isCycle(graph, dest, vis, stack)) {
+                return true;
+            }
         }
+        stack[curr] = false;
+        return false;
     }
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] inDeg = new int[numCourses];
-        CalInDeg(prerequisites, inDeg);
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 0; i<inDeg.length; i++) {
-            if(inDeg[i] == 0) {
-                q.offer(i);
+        ArrayList<Integer>[] graph = new ArrayList[numCourses];
+        for(int i = 0; i<numCourses; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for(int i = 0; i<prerequisites.length; i++) {
+            int dest = prerequisites[i][0];
+            int src = prerequisites[i][1];
+            graph[src].add(dest);
+        }
+        boolean[] vis = new boolean[numCourses];
+        boolean[] stack = new boolean[numCourses];
+        for(int i = 0; i<numCourses; i++) {
+            if(!vis[i]) {
+                if(isCycle(graph, i, vis, stack)) {
+                    return false;
+                }
             }
         }
-        int cnt = 0;
-        while(!q.isEmpty()) {
-            int curr = q.poll();
-            cnt++;
-            for(int i = 0; i<prerequisites.length; i++) {
-                if(prerequisites[i][1] == curr) {
-                    int dest = prerequisites[i][0];
-                    inDeg[dest]--;
-                    if(inDeg[dest] == 0) {
-                        q.offer(dest);
-                    }
-                } 
-            }
-        }
-        return cnt == numCourses;
+        return true;
     }
 }
