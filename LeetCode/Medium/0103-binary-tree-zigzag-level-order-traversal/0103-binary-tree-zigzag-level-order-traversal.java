67387d9277a9a18b0131637215454ee2f1c7948f
Rohan Tyagi <rohantyagi539@gmail.com>
2026-09-23 02:05:34 +0530

    Time: 1 ms (82.91%), Space: 43.5 MB (89.87%) - LeetHub

--- LeetCode/Medium/0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ LeetCode/Medium/0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -1,43 +1,26 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> zigzag = new ArrayList<>();
-        if(root == null) {
-            return zigzag;
-        }    
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) return ans;
         Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        boolean leftToRight = true;
+        q.offer(root);
+        boolean oddLvl = true;
         while(!q.isEmpty()) {
             int size = q.size();
-            LinkedList<Integer> lvl = new LinkedList<>();
+            LinkedList<Integer> list = new LinkedList<>();
             for(int i = 0; i<size; i++) {
-                TreeNode currNode = q.poll();
-                if(leftToRight) {
-                    lvl.addLast(currNode.val);
+                TreeNode curr = q.poll();
+                if(oddLvl) {
+                    list.addLast(curr.val);
                 } else {
-                    lvl.addFirst(currNode.val);
+                    list.addFirst(curr.val);
                 }
-                if(currNode.left != null) q.add(currNode.left);
-                if(currNode.right != null) q.add(currNode.right);
+                if(curr.left != null) q.offer(curr.left);
+                if(curr.right != null) q.offer(curr.right);
             }
-            leftToRight = !leftToRight;
-            zigzag.add(lvl);
+            oddLvl = !oddLvl;
+            ans.add(list);
         }
-        return zigzag;
+        return ans;
     }
 }
