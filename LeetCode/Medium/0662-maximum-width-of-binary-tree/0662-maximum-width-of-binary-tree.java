fa5967ba6a45b74bb044ba1733c8bd89b18674ee
Rohan Tyagi <rohantyagi539@gmail.com>
2026-09-23 03:36:18 +0530

    Time: 2 ms (42.06%), Space: 45.5 MB (8.03%) - LeetHub

--- LeetCode/Medium/0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ LeetCode/Medium/0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -5,11 +5,11 @@ class Solution {
         q.offer(new Pair<>(root, 0));
         int maxWidth = 0;
         while(!q.isEmpty()) {
-            int currLvlSize = q.size();
+            int currLvl = q.size();
             int stIdx = q.peekFirst().getValue();
             int endIdx = q.peekLast().getValue();
             maxWidth = Math.max(maxWidth, endIdx - stIdx + 1);
-            for(int i = 0; i<currLvlSize; i++) {
+            for(int i = 0; i<currLvl; i++) {
                 Pair<TreeNode, Integer> curr = q.poll();
                 TreeNode node = curr.getKey();
                 int idx = curr.getValue();
@@ -17,7 +17,7 @@ class Solution {
                     q.offer(new Pair<>(node.left, 2 * idx + 1));
                 }
                 if(node.right != null) {
-                    q.offer(new Pair<>(node.right, 2 * idx + 2));
+                    q.offer(new Pair<>(node.right, 2* idx + 2));
                 }
             }
         }
