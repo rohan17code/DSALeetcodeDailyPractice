2304be22b5014dbe56bdd5cb7fdbb003a6888fcc
Rohan Tyagi <rohantyagi539@gmail.com>
2026-09-25 01:28:28 +0530

    Time: 41 ms (67.35%), Space: 48.2 MB (7.98%) - LeetHub

--- LeetCode/Medium/1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ LeetCode/Medium/1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -25,23 +25,17 @@ class Solution {
         PriorityQueue<Pair> pq = new PriorityQueue<>();
         dist[0][0] = 0;
         pq.offer(new Pair(0,0,0));
-        
         int[][] directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
         while(!pq.isEmpty()) {
             Pair curr = pq.poll();
             int effort = curr.effort;
             int row = curr.row;
             int col = curr.col;
-
-            // destination
-            if(row == n - 1 && col == m - 1) {
-                return effort;
-            }
-            // checking 4 neighbours
+            if(row == n-1 && col == m-1) return effort;
             for(int i = 0; i<4; i++) {
                 int newRow = row + directions[i][0];
                 int newCol = col + directions[i][1];
-                if(newRow >= 0 && newRow < n && newCol >= 0 && newCol <m) {
+                if(newRow >= 0 && newRow < n &&  newCol >= 0 && newCol < m) {
                     int diff = Math.abs(heights[row][col] - heights[newRow][newCol]);
                     int newEffort = Math.max(effort, diff);
                     if(newEffort < dist[newRow][newCol]) {
